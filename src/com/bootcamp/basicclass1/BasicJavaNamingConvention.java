e275fdf69388e6ee620988a40863a91d12fd4620
Shankar Prasad Dahal <pdsdahal@gmail.com>
2023-04-29 08:11:58 +0545

    Local Variables Discussion

---
+++ src/com/bootcamp/basicclass1/BasicJavaNamingConvention.java
@@ -0,0 +1,11 @@
+package com.bootcamp.basicclass1;
+
+public class BasicJavaNamingConvention {
+
+	String firstName = "Ram";
+	String lastName = "Dahal";
+	String middleName = "Pd";
+	int age = 23;
+	
+	
+}
---
+++ src/com/bootcamp/basicclass1/LocalVariableDemo.java
@@ -0,0 +1,23 @@
+package com.bootcamp.basicclass1;
+
+public class LocalVariableDemo {
+
+	//main
+	public static void main(String[] args) {
+		
+		int age = 23;
+		String name = "Shankar Pd Dahal";
+		boolean flag = true;
+		double salary = 23.56;
+		
+		//syso
+		System.out.println("This is my first Class in Java");
+		System.out.println("Age : "+age);
+		System.out.println("Name : "+name);
+		System.out.println("Salary : "+salary);
+		
+		//syntax : "message "+variable_name+"message"+variable_name
+		
+		System.out.println("Name : "+name + " Age : "+age + " Salary : "+salary);
+	}
+}
